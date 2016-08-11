3c9cffff4c1e1b0ed26969604e7d09a824968dcf
JorjikS <evgeniya.pustovaya@gmail.com>
2016-08-11 15:43:39 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,127 @@
-# artillery-lambda
-Simple load testing with AWS Lambda
+# Reserve-performance
+
+## Goals
+This package deploys "artillery"-based lambda function to AWS Cloud and run pre-defined scenarios.
+
+## Environment Variables
+Make sure to add the following to your ~/.bash_profile and then reopen your terminal...
+
+```
+export NO_PROXY=$no_proxy
+export AWS_PROFILE="nordstrom-federated"
+export AWS_REGION="us-west-2"
+export PROXY=http://webproxysea.nordstrom.net:8181
+```
+
+If you want to see logs in console instead of "app.log" file use LOG_LOCATION=console environment variable
+
+## Setup
+
+1. Install [node](http://nodejs.org)
+2. Get the latest code from this repo
+3. If it is your first time to use this repo, run
+
+```
+$ setup
+```
+
+4. Otherwise, run
+
+```
+$ init
+```
+
+## Run Load test
+To run AWS cloud based load test you need to have AWS account. As input test options you need to define next:
+
+```
+var options = {
+        stackName: 'STACK_NAME',
+        functionName: 'LAMBDA_FUNCTION_NAME',
+        fileName: 'LAMBDA_ZIP_FILE_NAME',
+        s3Bucket: 'BUCKET_NAME',
+        s3Path: 'FOLDER_PATH_TO_ZIP',
+        prefix: prefix,
+        memorySize: 1024,       // optional, default value = 1024
+        runtime: 'nodejs4.3',   // optional, default value = 'nodejs4.3'
+        timeout: 300,           // optional, default value = 300
+        vpc: {
+            subnets: 'subnet-abcd1234,subnet-12ytrewq',
+            securityGroupIds: 'sg-qwerty12'
+        },
+        // roleArn: 'arn:aws:iam::123456789098:role/ANY/YOUR/PATH/LAMBDA_IAM_EXECUTION_ROLE',
+        rolePath: '/ANY/YOUR/PATH/', // optional, default value = ''
+        region: 'us-west-2',
+        account: '123456789098',
+        resourcePrefix: 'RESOURCE_PREFIX', // optional, default value = ''
+        scenarios: [{
+            name: 'FellingLucky',
+            metricNamespace: 'MyFunny-Feeling-lucky-test',
+            config: {
+                target: "http://google.com",
+                tls: {
+                    rejectUnauthorized: true
+                },
+                phases: [{
+                    arrivalRate: 70,
+                    duration: 10
+                }],
+                defaults: {
+                    headers: {
+                        'content-type': 'text/html; charset=UTF-8'
+                    }
+                }
+            },
+            scenarios: [{
+                flow: [{
+                    get: {
+                        url: '/search?q=feeling+lucky'
+                    }
+                }]
+            }]
+        }]
+};
+```
+
+If your test endpoints are not located in VPC you can undefine vpc param. Otherwise use for vpc.subnets and vpc.securityGroupIds comma-separated values or arrays.
+If IAM role you want to use for Lambda exists just pass its ARN in options as roleArn. Otherwise Lambda execution role will be created and removed with stack.
+Scenarios variable is regular Artillery-Core scenarios object with name and metricNamespace for better tracking AWS Cloudwatch metrics.
+
+### Run loadtest example which deploys Lambda-stack, runs load tests and removes Lambda-stack
+```
+var loadtest = require('artillery-lambda'),
+    options = {...}; // AWS configuration options for Lambda and scenarios
+
+return loadtest(options);
+```
+
+## Create stack once
+```
+var loadtest = require('artillery-lambda');
+
+function createStack() {
+    var options = {...}; // no need to specify scenarios here
+    return loadtest.deploy(options);
+}
+
+function runLoadtest() {
+    var options = {...}; // options with scenarios
+    return loadtest.runScenarios(options);
+}
+
+function removeStack() {
+    var options = {...}; // stackName only
+    return loadtest.undeploy(options);
+}
+```
+
+### Generate load from your local machine instead of Lambda
+With localRun = true load is artillery-core run pus CloudWatch metrics. All load is generated from running machine.
+
+```
+var options = {
+        localRun: true
+    },
+    loadtest = require('artillery-lambda');
+return loadtest(options);
+```
---
+++ index.js
@@ -0,0 +1,148 @@
+'use strict';
+
+var _ = require('lodash'),
+    Promise = require('bluebird'),
+    glob = require('glob-promise'),
+    fs = Promise.promisifyAll(require('fs')),
+    aws = require('./lib/util/aws.js'),
+    s3 = Promise.promisifyAll(new aws.S3()),
+    lambda = new aws.Lambda(),
+    cfn = require('cfn'),
+    logger = require('./lib/util/logger.js')('reserve-performance'),
+    localArtillery = require('./lib/function/index.js');
+
+function initOptions(options) {
+    return _.merge({
+        localRun: false,
+        memorySize: 1024,
+        runtime: 'nodejs4.3',
+        timeout: 300,
+        region: 'us-west-2',
+        rolePath: '/',
+        resourcePrefix: '',
+        fileName: ''
+    }, options);
+}
+
+function deploy(options) {
+    options = initOptions(options);
+
+    return glob(__dirname + '/lib/*.zip')
+        .then(function (files) {
+            options.fileName = files && files[0] && files[0].replace(/^.*[\\\/]/, '');
+            return fs.readFileAsync(files[0]);
+        })
+        .then(function (buffer) {
+            logger.debug('Uploading Lambda code archive to S3');
+            return s3.putObjectAsync({
+                Bucket: options.s3Bucket,
+                Key: options.s3Path + options.fileName,
+                Body: buffer
+            });
+        })
+        .then(function (s3result) {
+            logger.debug('Lambda code archive is uploaded to S3 ' + options.s3Bucket, s3result);
+        })
+        .then(function () {
+            logger.debug('Deploying performance stack to cloud..');
+            return cfn({
+                name: options.stackName,
+                template: __dirname + '/lib/formation/artilleryLambda.js',
+                params: options
+            });
+        });
+}
+
+// todo: add opportunity to run synchronous and asynchronous
+function runScenarios(options) {
+    logger.debug('Get promise to run scenarios.');
+
+    function runSingleScenario(scenario, runLocal) {
+        return new Promise(function (resolve, reject) {
+            function _success(data) {
+                logger.debug('Lambda successfully executed.');
+                resolve({
+                    name: scenario.name,
+                    data: data
+                });
+            }
+
+            function _failure(error) {
+                logger.debug('Lambda execution failed');
+                logger.debug(error);
+                reject(error);
+            }
+
+            function handleResult(err, data) {
+                logger.debug('Scenario finished.');
+                if (err) {
+                    console.log('error happened when calling Lambda!');
+                    return _failure(err);
+                }
+                return _success(data);
+            }
+
+            if (runLocal) {
+                localArtillery.handler(scenario, {}, handleResult);
+            } else {
+                lambda.invoke({
+                    FunctionName: options.functionName,
+                    Payload: JSON.stringify(scenario)
+                }, handleResult);
+            }
+        });
+    }
+
+    logger.debug('Running ' + options.scenarios.length + ' scenario(s)...');
+    return Promise.mapSeries(options.scenarios, function (scenario, index, length) {
+            logger.info('Running ' + (index + 1) + ' scenario of ' + length);
+            scenario.proxy = options.proxy;
+            return runSingleScenario(scenario, options.localRun);
+        })
+        .then(function (results) {
+            logger.debug('runScenarios is done:', results);
+            return {
+                artillery: results
+            };
+        });
+}
+
+function getStackTemplate(options) {
+    logger.debug('getting stack template for options:\n', options);
+
+    options = initOptions(options);
+    return require(__dirname + '/lib/formation/artilleryLambda.js')(options);
+}
+
+function undeploy(options) {
+    logger.debug('Undeploying performance stack...');
+    return cfn.delete(options.stackName);
+}
+
+function run(options) {
+    var promises = options.localRun
+            ? [runScenarios]
+            : [deploy, runScenarios, undeploy],
+        message = options.localRun
+            ? 'Local Run: Generating volume from local machine'
+            : 'Remote Run: Generating volume with Lambda calls: synchronous Lambda call for each separate scenario.';
+
+    options = initOptions(options);
+
+    logger.debug(message);
+
+    return Promise.mapSeries(promises, function (promise) {
+        return promise(options);
+    })
+        .then(function (results) {
+            results = _.flattenDeep(results);
+            return results.artillery;
+        });
+}
+
+run.deploy = deploy;
+run.undeploy = undeploy;
+run.runScenarios = runScenarios;
+run.getStackTemplate = getStackTemplate;
+
+module.exports = run;
